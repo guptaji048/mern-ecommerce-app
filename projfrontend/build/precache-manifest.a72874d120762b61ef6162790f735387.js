2f2e0ba7cf9c64a110129d992dcc60ad9625bc27
guptaji048 <guptaji048@gmail.com>
2020-05-13 22:42:41 +0530

    made responsive

---
+++ projfrontend/build/precache-manifest.a72874d120762b61ef6162790f735387.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb752d9c23a69204ce58c4d3cd2b8b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ec2abd04a25619229b3",
+    "url": "/static/css/main.91b5666a.chunk.css"
+  },
+  {
+    "revision": "4596a198d877ce0fbd9e",
+    "url": "/static/js/2.98bb8069.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.98bb8069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ec2abd04a25619229b3",
+    "url": "/static/js/main.081be1e4.chunk.js"
+  },
+  {
+    "revision": "8c804cdd4f2db9525a42",
+    "url": "/static/js/runtime-main.5c5f3968.js"
+  }
+]);
